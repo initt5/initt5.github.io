6c4a81477b4f02cb685915593e6d9a8859a41bad
initt6 <jckobkz@gmail.com>
2020-12-20 19:40:28 +0100

    [FIX] Fixed numbers overlapping

--- counter.js
+++ counter.js
@@ -1,114 +1,80 @@
 (function() {
     const form = document.forms.date;
-    const clock = document.querySelector('#clock');
-    let oldTime;
-    let timer;
-    // adds 0 to dates/hours which are < 10
-    function transformDate(h) {
-        let newH = '';
+    const clockElement = document.querySelector('#clock');
+    let oldTime = new Map();
+    let timerInterval;
+    function transformDate(date) {
+        let changeDate = '';
 
-        if (h < 10) {
-            newH = '0'+h;
+        if (date < 10) {
+            changeDate = '0'+date;
         }
         
         else {
-            newH = h;
+            changeDate = date;
         }
-        return String(newH)
+        return String(changeDate);
     }
 
-    function getNow() {
+    function getNowDate() {
         const date = new Date();
-        const today = `${date.getFullYear()}-${transformDate(date.getMonth()+1)}-${transformDate(date.getDate())}T${transformDate(date.getHours())}:${transformDate(date.getMinutes())}:${transformDate(date.getSeconds())}`;
-        return today
+        const nowDate = `${date.getFullYear()}-${transformDate(date.getMonth()+1)}-${transformDate(date.getDate())}T${transformDate(date.getHours())}:${transformDate(date.getMinutes())}:${transformDate(date.getSeconds())}`;
+        return nowDate
     }
-
-    // DO SECOND FUNCTION WHICH WILL SUPPORT CLONE AND NORMAL NODE!
-    function animateTransition(e, remove) { 
-        let velocity = parseInt(e.style.top);
-        e.id = `animated-${e.id}`;
-        let animation = setInterval(function() {
-            if (velocity <= -200) {
-                clearInterval(animation);
-                e.remove()
-                return
-            }
-            move();
-        }, 30);
-        function move() {
-            velocity -= 3;
-            e.style.top = `${velocity}px`;
-        }
+    function changeTime(entity) {
+        let timeContent = document.createElement('div');
+        let timeBox = document.querySelector(`#${entity[0]}-box`);
+        timeContent.innerHTML = `${transformDate(entity[1])}`;
+        timeContent.id = entity[0];
+        timeContent.style.top = '75px';
+        timeContent.className = 'content';
+        timeBox.appendChild(timeContent);
     }
-
-    //I'm sure that this can be done 10x times better
-    function countTime(value) {
-        const now = new Date(getNow());
-        const date = new Date(value);
-        const difference = date-now;
-        const weeks = Math.floor(difference / (1000*3600*24*7));
-        const days = Math.floor((difference % (1000*3600*24*7)) / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-        const time = new Map([['weeks', weeks], ['days', days], ['hours', hours], ['minutes', minutes], ['seconds', seconds]]);
+    function countTime(formValue) {
+        const now = new Date(getNowDate());
+        const date = new Date(formValue);
+        const timeDifference = date-now;
+        const weeks = Math.floor(timeDifference / (1000*3600*24*7));
+        const days = Math.floor((timeDifference % (1000*3600*24*7)) / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const presentTime = new Map([['weeks', weeks], ['days', days], ['hours', hours], ['minutes', minutes], ['seconds', seconds]]);
         if (weeks >= 0 && days >= 0 && minutes >= 0 && seconds >= 0 && hours >= 0) {
             const node = document.querySelector('.time');
             if (!node) {
 
-                for (let entity of time) {
+                for (let entity of presentTime) {
                     let timeElement = document.createElement('div');
                     timeElement.className = 'time';
                     timeElement.id = `${entity[0]}-box`;
-                    clock.appendChild(timeElement);
-                    oldTime = new Map();
+                    clockElement.appendChild(timeElement);
                 }
             }
-            for (let entity of time){
+            for (let entity of presentTime){
                 if (oldTime.get(entity[0]) != entity[1]) {
                     if (document.querySelector(`#${entity[0]}`)){
                         let previousElement = document.querySelector(`#${entity[0]}`);
                         if (previousElement.classList.contains('content')) {
-                            console.log(previousElement);
+                            setTimeout(() => previousElement.remove(), 1000);
                             previousElement.className = 'animate';
                         }
                     }
-                    let timeContent = document.createElement('div');
-                    let box = document.querySelector(`#${entity[0]}-box`);
-                    timeContent.innerHTML = `${transformDate(entity[1])}`;
-                    timeContent.id = entity[0];
-                    timeContent.style.top = '75px';
-                    timeContent.className = 'content';
-                    box.appendChild(timeContent);
+                    if (!document.hidden) {
+                        changeTime(entity);
+                    } else {
+                        if (entity[0] != 'seconds') {
+                            changeTime(entity);
+                        }
+                    }
                 }
             }
-            
-                    /*clock.appendChild(timeElement);
-                    let timeContent = document.createElement('div')
-                    timeContent.innerHTML = `${transformDate(entity[1])}`;
-                    timeContent.className = 'content animate'
-                    timeContent.style.top = '0px';
-                    timeContent.id = entity[0];
-                    timeElement.id = `${entity[0]}-box`;
-                    timeElement.appendChild(timeContent);*/
-                /*for (let entity of time) {
-                    if (oldTime.get(entity[0]) != entity[1]) {
-                        let timeBit = document.querySelector(`#${entity[0]}`);
-                        let nextTimeElement = document.createElement('div');
-                        nextTimeElement.innerHTML = `${transformDate(entity[1])}`;
-                        nextTimeElement.id = entity[0];
-                        nextTimeElement.className = `content`;
-                        nextTimeElement.style.top = '85px';
-                        animateTransition(nextTimeElement, true);
-                        document.querySelector(`#${entity[0]}-box`).append(nextTimeElement);
-                    }
-                }*/
-                oldTime = new Map(time);
+                oldTime = new Map(presentTime);
 
         }
         if (weeks == 0 && days == 0 && minutes == 0 && seconds == 0 && hours == 0) {
             localStorage.removeItem('date');
-            clock.innerHTML = '';
+            clockElement.innerHTML = '';
         }
     }
     window.addEventListener('animationend', () => {
@@ -125,24 +91,24 @@
         }
     });
     form.button.addEventListener('click', () => {
-        const date = form.datetime.value;
+        const date = form.datetime.formValue;
         if (!localStorage.getItem('date')) {
             localStorage.setItem('date', date);
-            timer = setInterval(() => countTime(date), 500);
+            timerInterval = setInterval(() => countTime(date), 500);
         } else {
             localStorage.removeItem('date');
             localStorage.setItem('date', date);
-            clearInterval(timer);
-            timer = setInterval(() => countTime(date), 500);
+            clearInterval(timerInterval);
+            timerInterval = setInterval(() => countTime(date), 500);
 
         }
     });
     window.addEventListener('load', () => {
         const date = localStorage.getItem('date');
         if (localStorage.getItem('date') != null) {
-            form.datetime.value = date;
-            clearInterval(timer);
-            timer = setInterval(() => countTime(date), 500);
+            form.datetime.formValue = date;
+            clearInterval(timerInterval);
+            timerInterval = setInterval(() => countTime(date), 500);
         } 
     });
 })()
